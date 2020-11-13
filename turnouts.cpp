54565b7c5651e253a7656febc69f798a381df9d1
bask185 <sebastiaan_knippels4@hotmail.com>
2020-11-13 21:35:14 +0100

    compiled

--- turnouts.cpp
+++ turnouts.cpp
@@ -45,7 +45,7 @@ void initTurnouts() {
 	turnout[2].lowPos = 110 ; turnout[2].highPos =  70 ; turnout[2].state = 1 ;
 	turnout[3].lowPos =  70 ; turnout[3].highPos = 110 ; turnout[3].state = 1 ;
 	turnout[4].lowPos =  70 ; turnout[4].highPos = 110 ; turnout[4].state = 1 ;
-	turnout[5].lowPos =  70 ; turnout[5].highPos = 110 ; turnout[5].state = 1 ;
+	turnout[5].lowPos = 110 ; turnout[5].highPos =  70 ; turnout[5].state = 1 ;
 	turnout[6].lowPos = 110 ; turnout[6].highPos =  70 ; turnout[6].state = 1 ;
 
 	for(byte j = 0 ; j < 7 ; j ++ ) {
